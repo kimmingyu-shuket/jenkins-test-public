aa98b1635f9f912de3156fe0439b4f75e87e36f0
Kimmingyu-shuket <mg.kim@shuket.company>
2022-11-28 11:47:11 +0900

    test 7

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ console.log("push test3");
 console.log("push test4");
 console.log("push test5");
 console.log("push test6");
+console.log("push test7");
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
