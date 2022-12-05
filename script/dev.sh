ebe30323899bff3992a8903b3cfeed935a981c70
Kimmingyu-shuket <mg.kim@shuket.company>
2022-12-05 11:10:15 +0900

    copy test

--- script/dev.sh
+++ script/dev.sh
@@ -2,5 +2,5 @@
 
 echo "dev.sh print test"
 echo "dev.sh copy test"
-cp -r /var/lib/jenkins/workspace/CICD_TEST /var/www/CICD_TEST
+sudo cp -r /var/lib/jenkins/workspace/CICD_TEST /var/www/CICD_TEST
 cd /var/www
